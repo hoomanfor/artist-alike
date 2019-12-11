f653359d4613b96cae79f5d287c8a3156b3d3d19
Hoo Man <hoomanfor@gmail.com>
2019-12-11 18:36:59 -0500

    nested for loop to iterate through each answer within the artists objects

--- app/data/artists.js
+++ app/data/artists.js
@@ -3,15 +3,15 @@ var artists = [
         "name": "Carson McCullers",
         "photo": "https://via.placeholder.com/200x200.png",
         "answers":[
-            5,
+            2,
             1,
-            4,
-            4,
+            2,
+            2,
             5,
             1,
             2,
-            5,
-            4,
+            1,
+            2,
             1
         ]
     },
@@ -28,6 +28,38 @@ var artists = [
             2,
             5,
             4,
+            1
+        ]
+    },
+    {
+        "name": "Bill Callahan",
+        "photo": "https://via.placeholder.com/200x200.png",
+        "answers":[
+            1,
+            1,
+            1,
+            1,
+            2,
+            1,
+            1,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Salvador Dalí",
+        "photo": "https://via.placeholder.com/200x200.png",
+        "answers":[
+            2,
+            1,
+            1,
+            4,
+            4,
+            5,
+            5,
+            5,
+            2,
             1
         ]
     }
