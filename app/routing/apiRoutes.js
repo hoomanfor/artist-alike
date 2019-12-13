6e9ad6382b1e44a96d943f77c039ba1fd942f12c
Hoo Man <hoomanfor@gmail.com>
2019-12-13 17:16:31 -0500

    compatibility logic is now on server side post to api/artist

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,24 @@ module.exports = function(app) {
         response.json(artists);
     })
     app.post("/api/artists", function(request, response) {
-        var test = request.body;
-        console.log("This works", request.body);
-        response.json({ works: true });
+        var user = request.body;
+        var compArr = [];
+        var sum = 0;
+        for (var i = 0; i < artists.length; i++) {
+            for (var j = 0; j < artists[i].answers.length; j++) {
+                var dif = artists[i].answers[j] - user.answers[j];
+                dif = Math.abs(dif);
+                sum += dif;
+                if (j == 9) {
+                    compArr.push(sum);
+                    sum = 0; 
+                }
+            }
+        }
+        var mostCompSum = Math.min(...compArr);
+        var mostCompIndex = compArr.indexOf(mostCompSum);
+        var matchName = artists[mostCompIndex].name;
+        var matchPhoto = artists[mostCompIndex].photo;
+        response.json({ match_name: matchName, match_photo: matchPhoto });
     })
 }
